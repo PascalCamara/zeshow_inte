252d914b1e7412db89fb4a49eceeef0ac9dbb759
CAMARA Pascal <camara@MacBook-Pro-de-CAMARA.local>
2017-02-20 22:25:21 +0100

    header

--- layouts/header.php
+++ layouts/header.php
@@ -7,32 +7,51 @@
 
 
     <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
 <body>
 
-<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded bg-black">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div>
+        <img class="logo" src="./img/logo.png" />
+    </div>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
             </li>
         </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+
+        <div class="item-nav">
+            <img class="mon-compte" src="./img/acount.png" alt="">
+            <a class="nav-link link-nav" href="#">Mon compte</a>
+        </div>
+
+
+        <div class="item-nav">
+            <a class="btn btn-outline-secondary btn-outline-orange " href="#">Accès recruteur</a>
+        </div>
     </div>
 </nav>
 
 
+
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded bg-gris">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="second-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Notre objectif</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Mes outils</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Toutes les offres</a>
+            </li>
+        </ul>
+    </div>
+</nav>
