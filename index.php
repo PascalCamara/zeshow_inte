84a632559e7fcae1896ed83ac66caa2ba2b638ae
CAMARA Pascal <camara@MacBook-Pro-de-CAMARA.local>
2017-02-20 23:01:32 +0100

    pull hugo

--- index.php
+++ index.php
@@ -1,5 +1,36 @@
 <?php  include "layouts/header.php"; ?>
 
+    <div class="jom-index" id="jom-index">
+        <div class="container">
+            <form action="" class="form-index">
+                <input type="text" placeholder="Mots clés" class="element-form-index">
+                <input type="text" placeholder="Villen Code postal" class="element-form-index">
+                <a class="btn-search" href="#">Rechercher</a>
+            </form>
+        </div>
+    </div>
 
+    <div class="section" style="background-color: #e6e6e6;">
+
+        <div class="container">
+            <p class="h2">Ils font appel à nos services</p>
+
+            <div class="row" style="margin-top: 65px;
+    display: inline-block;
+    text-align: center;
+    width: 100%;">
+                <div class="item-service">
+                </div>
+                <div class="item-service">
+                </div>
+                <div class="item-service">
+                </div>
+                <div class="item-service">
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 
 <?php  include "layouts/footer.php"; ?>
